d98535c76b6215a537003105d663d35256acb937
Ahmed Yosry <amu3dev@gmail.com>
2023-02-14 12:05:49 +0200

    Added Bootstrap CSS and Bootstrap Bundle JS.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import "./index.css";
 import App from "./App";
 
 // // Bootstrap CSS
-//import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 // // Bootstrap Bundle JS
-//import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
