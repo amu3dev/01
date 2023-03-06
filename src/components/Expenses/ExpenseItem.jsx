5a9c9098844fbd8f655348db8e5a8776b6af71c5
Ahmed Yosry <amu3dev@gmail.com>
2023-03-06 12:19:15 +0200

    - Rollback button 'update'

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,22 +1,22 @@
-import React, { useState } from "react";
+import React from "react"; // { useState }
 import ExpenseDate from "./ExpenseDate";
 import "../../css/ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const { amount, date } = props;
-  const [title, setTitle] = useState(props.title);
-  const handelClick = () => {
-    setTitle("Item updated!");
-    console.log(title);
-  };
+  const { amount, date, title } = props;
+  // const [title, setTitle] = useState(props.title);
+  // const handelClick = () => {
+  //   setTitle("Item updated!");
+  //   console.log(title);
+  // };
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
-        <button onClick={handelClick}>Update</button>
+        {/* <button onClick={handelClick}>Update</button> */}
       </div>
     </Card>
   );
