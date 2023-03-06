55ca9995a1b4fe4a9125060156e3947d28d1e4ca
Ahmed Yosry <amu3dev@gmail.com>
2023-03-06 12:27:11 +0200

    - Rollback send selectedYear

--- src/components/NewExpense/ExpensesFilter.jsx
+++ src/components/NewExpense/ExpensesFilter.jsx
@@ -1,6 +1,9 @@
 import "../../css/ExpensesFilter.css";
 
-const ExpensesFilter = ({ onYearChange, selectedYear }) => {
+const ExpensesFilter = ({
+  onYearChange,
+  // selectedYear
+}) => {
   const handelYearSelected = (event) => {
     // console.log("ExpenseFilter.jsx");
     // console.log(event.target.value);
@@ -11,7 +14,10 @@ const ExpensesFilter = ({ onYearChange, selectedYear }) => {
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select value={selectedYear} onChange={handelYearSelected}>
+        <select
+          // value={selectedYear}
+          onChange={handelYearSelected}>
+          <option value="">Years</option>
           <option value="2023">2023</option>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
