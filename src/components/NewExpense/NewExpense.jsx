234de0e122d366e7915dd197f27a4ca3b972f79b
Ahmed Yosry <amu3dev@gmail.com>
2023-03-06 12:24:34 +0200

    - Added expenseFormAppearance

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../css/NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = ({ onSaveExpenseData }) => {
+  const [expenseFormAppearance, setExpenseFormAppearance] = useState(false);
+  const HandleExpenseAppear = () => {
+    setExpenseFormAppearance(false);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={onSaveExpenseData} />
+      {expenseFormAppearance && (
+        <ExpenseForm
+          onSaveExpenseData={onSaveExpenseData}
+          onExpenseAppear={HandleExpenseAppear}
+        />
+      )}
+      {!expenseFormAppearance && (
+        <button
+          onClick={() => {
+            setExpenseFormAppearance(true);
+          }}>
+          Add New Expense
+        </button>
+      )}
     </div>
   );
 };
